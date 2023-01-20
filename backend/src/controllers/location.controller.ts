a7cb05306da486be891c113a278359946ffb1cd8
Martin Fernandez <43618471+DeveloperMartin@users.noreply.github.com>
2023-01-19 23:13:04 -0300

    add(backend): LocationController class

---
+++ backend/src/controllers/location.controller.ts
@@ -0,0 +1,9 @@
+import { CreateLocationDTO, UpdateLocationDTO } from "../models/DTO/location.dto";
+import { locationService } from "../services/location.service";
+import { Body, Get, Path, Post, Put, Query, Route, Tags } from "tsoa";
+
+@Route('location')
+@Tags('Location')
+export class LocationController {
+
+}
