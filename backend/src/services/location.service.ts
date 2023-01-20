4d574c1903dba7ed3881fb873b165ea0383174ca
Martin Fernandez <43618471+DeveloperMartin@users.noreply.github.com>
2023-01-19 23:11:39 -0300

    add(backend): location service

---
+++ backend/src/services/location.service.ts
@@ -0,0 +1,6 @@
+import { CreateLocationDTO, UpdateLocationDTO } from "../models/DTO/location.dto";
+import prisma from "../models";
+class LocationService {
+}
+
+export const locationService = new LocationService()
