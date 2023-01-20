2ccb979627d2f04343c14242c369b301cce8dee3
Martin Fernandez <43618471+DeveloperMartin@users.noreply.github.com>
2023-01-19 23:13:24 -0300

    add(backend): add getLocationListController

--- backend/src/controllers/location.controller.ts
+++ backend/src/controllers/location.controller.ts
@@ -5,5 +5,12 @@ import { Body, Get, Path, Post, Put, Query, Route, Tags } from "tsoa";
 @Route('location')
 @Tags('Location')
 export class LocationController {
+  @Get('get-location-list')
+  public async getLocationList(
+    @Query() isActive?: boolean
+  ){
+    return await locationService.getLocationList(isActive)
+  }
+
 
 }
