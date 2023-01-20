ab144ba93813b8fdef95dc4433301b91141bb99d
Martin Fernandez <43618471+DeveloperMartin@users.noreply.github.com>
2023-01-19 23:12:16 -0300

    add(backend): getLocationList method in location service

--- backend/src/services/location.service.ts
+++ backend/src/services/location.service.ts
@@ -1,6 +1,21 @@
 import { CreateLocationDTO, UpdateLocationDTO } from "../models/DTO/location.dto";
 import prisma from "../models";
 class LocationService {
+  async getLocationList(isActive: boolean = true) {
+    const location = await prisma.location.findMany({
+      where: {
+        isActive: isActive
+      },
+      select: {
+        locationId: true,
+        code: true,
+        description: true
+      }
+    })
+
+    return location
+  }
+
 }
 
 export const locationService = new LocationService()
