01152b875f7049028e414d8163aa3a27a9a2426a
Martin Fernandez <43618471+DeveloperMartin@users.noreply.github.com>
2023-01-25 13:03:55 -0300

    add(backend): getCategoryList method in category service

--- backend/src/services/category.service.ts
+++ backend/src/services/category.service.ts
@@ -3,6 +3,21 @@ import { UpdateCategoryDTO, CreateCategoryDTO } from '../models/DTO/category.dto
 
 
 class CategoryService {
+  async getCategoryList(isActive: boolean = true) {
+    const categories = await prisma.category.findMany({
+      where: {
+        isActive: isActive
+      },
+      select: {
+        categoryId: true,
+        code: true,
+        description: true
+      }
+    })
+
+    return categories
+  }
+
 }
 
 export const categoryService = new CategoryService()
