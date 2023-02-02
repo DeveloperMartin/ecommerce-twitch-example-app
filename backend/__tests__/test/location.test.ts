87aceea321aa974c0816cd6717519f0c1ea28a82
Martin Fernandez <43618471+DeveloperMartin@users.noreply.github.com>
2023-02-02 20:13:04 -0300

    add(backend): getCategoryByCode test case

--- backend/__tests__/test/location.test.ts
+++ backend/__tests__/test/location.test.ts
@@ -1,4 +1,5 @@
 import { locationService } from '../../src/services/location.service'
+import { categoryService } from '../../src/services/category.service'
 import { prismaMock } from '../database-simulator/singleton.simulator'
 
 test('create new location', async () => {
@@ -19,3 +20,38 @@ test('create new location', async () => {
     description: 'location-description-test',
   })).resolves.toEqual(location)
 })
+
+
+describe('getCategoryByCode', () => { 
+  it('Should return a category with the specified code', async () => {
+    const mockCategory = {
+      categoryId: 1,
+      code: 'CAT1',
+      description: 'Test Category 1',
+      isActive: true
+    };
+
+    const findUniqueMock = jest.fn().mockReturnValue(mockCategory);
+    (prismaMock.category.findUnique as any) = findUniqueMock;
+
+  
+
+    const category = await categoryService.getCategoryByCode('CAT1');
+    expect(category).toEqual(mockCategory);
+    expect(category?.code).toEqual('CAT1');
+    // expect(findUniqueMock).toHaveBeenCalledWith({
+    //   where: {
+    //     code: 'CAT1'
+    //   },
+    //   select: {
+    //     categoryId: true,
+    //     code: true,
+    //     description: true,
+    //     isActive: true
+    //   }
+    // });
+
+  })
+ })
+
+
